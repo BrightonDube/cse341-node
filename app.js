7581954ee46c3ea90173bebbc08669a5c628d050
Brighton <BrightonDube@users.noreply.github.com>
2025-03-10 10:22:54 +0200

    added connect.js

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+
 const express = require('express');
-const app = express();
 const router = require('./routes');
 require('dotenv').config();
-
+const connectDB = require('./db/connect'); 
+const app = express();
 const port = process.env.PORT || 5000;
 const mongodbAtlasUri = process.env.MONGODB_ATLAS_URI;
-const mongoose = require('mongoose');
 
 app.use('/', router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(mongodbAtlasUri)
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('MongoDB Atlas connection error:', err));
-app.listen(port, () => {
-  console.log('Web Server is listening at port ' + port);
-});
+// Connect to the database
+const start = async () => {
+    try {
+        await connectDB(mongodbAtlasUri);  
+        console.log('Connected to MongoDB Atlas');
+        app.listen(port, () => {
+            console.log('Web Server is listening at port ' + port);
+        });
+    } catch (err) {
+        console.error('MongoDB Atlas connection error:', err);
+    }
+};
+
+start();
